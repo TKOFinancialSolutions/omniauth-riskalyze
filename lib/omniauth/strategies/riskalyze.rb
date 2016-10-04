d0377306328895ebdf5ea3414bcd6c9fcdd8bd4b
Kevin Pheasey <kevin@kpheasey.com>
2016-10-04 12:33:18 -0400

    remove raw info logic

--- lib/omniauth/strategies/riskalyze.rb
+++ lib/omniauth/strategies/riskalyze.rb
@@ -13,31 +13,6 @@ module OmniAuth
           authorize_url: 'https://pro.riskalyze.com/oauthconnect',
           token_url: 'https://api2.riskalyze.com/ap/v1/oauthpro/token'
       }
-
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
-      uid { raw_info['id'] }
-
-      info do
-        {
-            name: raw_info['name'],
-            email: raw_info['email']
-        }
-      end
-
-      extra do
-        {
-            'raw_info' => raw_info
-        }
-      end
-
-      def raw_info
-        log :debug, access_token.inspect
-        @raw_info ||= access_token.get('/me').parsed
-      end
     end
   end
 end
